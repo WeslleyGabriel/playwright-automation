375983c2c9e6ed2546907a8cc23dbd48a6bfc97b
Weslley Gabriel <you@example.com>
2025-01-24 17:12:26 -0300

    simple changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,14 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
+
+    launchOptions: {
+      slowMo: 300
+    },
+
+    video: 'on',
+    screenshot: 'only-on-failure',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
